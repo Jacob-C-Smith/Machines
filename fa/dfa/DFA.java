505f606b85dacc12863040e6761599ea53c31b01
Jacob Smith <jacobsmithsinbox@gmail.com>
2024-09-05 18:49:35 -0600

    [DFA] Implemented accessors and mutators
    Co-Authored-By: JonFlores3475 <142841049+jonflores3475@users.noreply.github.com>

--- fa/dfa/DFA.java
+++ fa/dfa/DFA.java
@@ -1,35 +1,95 @@
 // Package
 package fa.dfa;
 
-// Imports
+// Java standard library
+import java.util.Map;
 import java.util.Set;
+import java.util.HashSet;
+import java.util.HashMap;
+
+// Machines
 import fa.State;
 
 // Classes
 public class DFA implements DFAInterface {
 
+    // Private fields
+    private Set<Character> sigma = null;
+    private Map<String, State> states = null;
+    private State initialState = null;
+    private Set<State> finalStates = null;
+    
+    public DFA ( )
+    {
+
+        // Construct an alphabet
+        this.sigma = new HashSet<Character>();
+
+        // Construct a collection of states
+        this.states = new HashMap<String, State>();
+
+        // Construct a collection of the final states
+        this.finalStates = new HashSet<State>();
+
+        // Done
+    }
+
     @Override
     public boolean addState(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addState'");
+        
+        // Initialized data
+        boolean ret = ( states.get(name) == null );
+        
+        // If the state isn't there ...
+        if ( ret )
+                
+            // ... add it to the state collection
+            states.put(name, null);
+        
+        // Done
+        return ret;
     }
 
     @Override
     public boolean setFinal(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setFinal'");
+        
+        // Initialized data
+        State state = states.get(name);
+
+        // Error checking
+        if ( state == null ) return false;
+
+        // Add the state to the final state collection
+        finalStates.add(state);
+
+        // Done
+        return true;
     }
 
     @Override
     public boolean setStart(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setStart'");
+        
+        // Initialized data
+        State state = states.get(name);
+
+        // Error checking
+        if ( state == null ) return false;
+
+        // Set the initial state
+        initialState = state;
+
+        // Done
+        return true;
     }
 
     @Override
     public void addSigma(char symbol) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addSigma'");
+
+        // Add the symbol to the alphabet
+        sigma.add(symbol);
+
+        // Done
+        return;
     }
 
     @Override
@@ -40,26 +100,42 @@ public class DFA implements DFAInterface {
 
     @Override
     public Set<Character> getSigma() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getSigma'");
+        
+        // Initialized data
+        HashSet<Character> ret = new HashSet<Character>();
+
+        // Iterate through our set
+        for (Character character : sigma) 
+
+            // Add the character to the (copy of the) alphabet
+            ret.add(character);
+        
+        // Done
+        return ret;
     }
 
     @Override
     public State getState(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getState'");
+        
+        // Done
+        return states.get(name);
     }
 
     @Override
     public boolean isFinal(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isFinal'");
+            
+        // Initialized data
+        boolean ret = finalStates.contains(name);
+        
+        // Done
+        return ret;    
     }
 
     @Override
     public boolean isStart(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isStart'");
+        
+        // Done
+        return ( initialState.getName().equals(name) );
     }
 
     @Override
