ecbdcfc95a364dcf27eb2e634f69725b4de911e1
Jacob Smith <jacobsmithsinbox@gmail.com>
2024-12-05 14:33:03 -0700

    [Simulator] Refactored simulator

--- tm.java
+++ tm.java
@@ -1,15 +1,29 @@
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class tm {
 
     public static void main(String[] args) {
-        Machine tm;
-        try {
-            tm = new Machine("file2.txt");
-            System.out.printf(tm.toString());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+
+        // Initialized data
+        Machine tm = null;
+        String file = "file5.txt";
+        
+        // Print the path to the Turing machine file
+        System.out.printf("Simulating %s\n", file);
+
+        // Construct the turing machine
+        try { tm = new Machine(file); }
+        
+        // Catch errors
+        catch ( FileNotFoundException e ) { e.printStackTrace(); }
+
+        // Run the turing machine
+        while ( tm.isHalted() == false )
+            tm.step();
+        
+        // Print the sum of the tape
+        System.out.printf("Tape sum: %d\n",tm.tapeSum());
     }
 }
 
