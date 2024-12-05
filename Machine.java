2f053ca0e2e62d1c5dfc787522e8f65a543af225
Jacob Smith <jacobsmithsinbox@gmail.com>
2024-12-05 14:28:13 -0700

    [Machine] Finished Turing machine parser

--- Machine.java
+++ Machine.java
@@ -1,22 +1,27 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Machine
 {
-    private TapeInterface t;
-    private ArrayList<Transition> r;
-    char alphabet[];
-    char transitions[/* on symb */][/* from state */];
 
+    // Constants
+    private final int INDEX_WRITE     = 0;
+    private final int INDEX_TO        = 1;
+    private final int INDEX_DIRECTION = 2;
+
+    // Private fields
+    private char alphabet[];
+    private char transitions[/* on symb */][/* from state */][/* 3-tuple: (write, to, direction) */];
     
-    /*
-     c : char = read()
-     curr_state = transitions[c][curr_state]
-     */
+    private TapeInterface t;
+    private boolean halt;
+    private int steps = 0;
 
+    private int current_state = 0;
+    private int final_state;
+    
     /** !
      * Construct a Turing machine from a plaintext file
      * 
@@ -30,66 +35,74 @@ public class Machine
         
         // Uninitialized data
         int state_quantity = 0;
+        int alphabet_size = 0;
+        Pattern old_delimiter;
+        String string = "";
 
         // Initialized data
         File file = new File(path);
         Scanner scanner = new Scanner(file);
+        
+        // Construct the tape
+        t = new TapeA(string.toCharArray());
 
         // Store the quantity of states
         state_quantity = scanner.nextInt();
+
+        // Store the final state
+        final_state = state_quantity - 1;
         
         // Advance to the next line
         scanner.nextLine();
 
         // Parse the alphabet
-        alphabet = scanner.nextLine().toCharArray();
-
-        // Allocate the transition LUT
-        transitions = new char[alphabet.length][state_quantity];
-
-        // // Construct transition LUT
-        // while ( scanner.hasNextLine() ) {
-        //    
-        // }
-
-        // Uninitialized data
-        Pattern old_delimiter;
+        alphabet_size = scanner.nextInt();
         
+        // Advance to the next line
+        scanner.nextLine();
+
+        // Construct the alphabet array
+        alphabet = new char[alphabet_size];
+
+        for (int i = 0; i < alphabet.length; i++)
+            alphabet[i] = (char) i;
+        
+        // Allocate the transition LUT
+        transitions = new char[alphabet.length+1][state_quantity-1][3];
+
         // Push the scanner delimiter
         old_delimiter = scanner.delimiter();
 
         // Set the scanner delimiter to a comma
         scanner.useDelimiter(",");
 
-        //
-        System.out.printf("(FROM, TO, ON, READ, WRITE, DIRECTION)\n");
-
         // Parse transitions
+        for (int i = 0; i < state_quantity - 1; i++)
         {
-            for (int i = 0; i < state_quantity - 1; i++)
+            for (int j = 0; j < alphabet.length + 1; j++)
             {
-                for (int j = 0; j < alphabet.length + 1; j++)
-                {
-                    
-                    Scanner s = new Scanner(scanner.nextLine());
-                    s.useDelimiter(",");
+                
+                // Initialized data
+                Scanner s = new Scanner(scanner.nextLine());
+                int to = 0;
+                int wr = 0;
+                char d = 0;
+                
+                // Set the delimiter
+                s.useDelimiter(",");
 
-                    int to = s.nextInt();
-                    int wr = s.nextInt();
-                    char d = ( s.next().charAt(0) == 'L' ) ? 'L' : 'R';
+                // Parse the (to, write, direction) tuple 
+                to = s.nextInt();
+                wr = s.nextInt();
+                d = s.next().charAt(0);
 
-                    // Print each transition
-                    System.out.printf("(%d   , %d , %d , %d   , %d    , %c)\n",
-                        i,
-                        to,
-                        j,
-                        j,
-                        wr,
-                        d
-                    );
-
-                    s.close();
-                }
+                // Store the (to, write, direction) tuple 
+                transitions[j][i][INDEX_WRITE]     = (char) wr;
+                transitions[j][i][INDEX_TO]        = (char) to;
+                transitions[j][i][INDEX_DIRECTION] = d;
+                
+                // Clean up
+                s.close();
             }
         }
 
