1cc4678767e8e0b57894a58af441ee78a994745c
Jacob Smith <jacobsmithsinbox@gmail.com>
2024-12-05 14:30:53 -0700

    [Tape] Refactored Tape to use +/- arrays

--- TapeA.java
+++ TapeA.java
@@ -1,23 +1,33 @@
 public class TapeA implements TapeInterface {
 
-    private char[] l;
+    private char[] pl;
+    private char[] nl;
+    private int lim;
+
     private int head = 0;
 
     public TapeA ( char[] tape )
     {
-        l = tape;
+        //lim = 24576;
+        lim = 135;
+        pl = new char[lim];
+        nl = new char[lim];
+
+        for (int i = 0; i < tape.length; i++)
+            pl[i] = (char)(tape[i]-'0');
     }
 
     @Override
     public char read ( )
     {
-        return l[head];
+        return (head >= 0) ? pl[head] : nl[-head - 1];
     }
 
     @Override
     public void write ( char c )
     {
-        l[head] = c;
+        if   (head >= 0) pl[head]      = c;
+        else             nl[-head - 1] = c;
     }
     
     @Override
